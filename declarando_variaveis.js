90cadf3ff7fe2081d5c4dc855550ed8cacf0e217
Rhennan Carvalho <ascomfrc@gmail.com>
2026-04-19 01:53:39 -0300

    Enviando especificamente o arquivo de declaracao de variaveis

---
+++ declarando_variaveis.js
@@ -0,0 +1,8 @@
+//Var declaraçãoo antiga, escopo de função que pode ser declarada
+var name = "Jhon";
+
+// let escopo de bloco, não pode ser redeclarada, pode ser atribuida
+let age = 25;
+
+// const escopo de bloco, não pode ser redeclarada nem retribuida
+const PI = 3.14159;
